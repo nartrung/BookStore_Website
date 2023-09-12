d453f5e9759eaf7183d6a90c72bd2bba962bbac4
nartrung <nartrung02@gmail.com>
2023-09-12 22:01:33 +0700

    config nuxt file

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -1,7 +1,9 @@
+
+const URL = "http://localhost:3000";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ad',
+    title: 'Admin Page',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -9,8 +11,28 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/font-awesome/css/all.css' },
+      { rel: 'stylesheet', href: '/css/default.css' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' }
+    ],
+    script: [
+      {
+          type: 'text/javascript',
+          src: 'https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js',
+          body: true
+      },
+      {
+        type: 'text/javascript',
+        src: 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',
+        body: true
+      },
+      {
+        type: 'text/javascript',
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js',
+        body: true
+    },
+  ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -41,7 +63,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: URL,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
@@ -53,5 +75,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  server:{
+    port: 3001,
   }
 }
